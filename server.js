f6cebb548a9a04371a28bf9d33a6c0212d713018
Ubuntu <ubuntu@ip-172-31-44-109.eu-west-1.compute.internal>
2020-06-28 15:44:32 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const owmApiKey = '5badf62e6c47455b3c449f1af9635254';
+const googleApiKey = 'AIzaSyB-3hW5ZIn9ErVs27FdQBRFXQ6CTe0QJ_Y';
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let d = new Date();
+let n = d.getDay();
+let dayAfter = daysOfWeek[n + 2]
+var lat, lon, locationName;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  //let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  
+  let locationurl = `https://maps.googleapis.com/maps/api/geocode/json?components=administrative_area:${city}&key=${googleApiKey}`;
+
+  request(locationurl, function(err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      console.log("using google api..");
+      let location = JSON.parse(body);
+      lat = location.results[0].geometry.location.lat;
+      lon = location.results[0].geometry.location.lng;
+      locationName = location.results[0].address_components[0].long_name;
+      console.log("lat is: " + lat);
+      console.log("lon is: " + lon);
+    }
+
+    let weatherurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${owmApiKey}`;
+
+    request(weatherurl, function (err, response, body) {
+      console.log("Weather request. Lat is " + lat + " and lon is " + lon);
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body);
+        if(weather.timezone == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let currentTempK = weather.current.temp;
+          let currentTempC = currentTempK - 273.15;
+          currentTempC = currentTempC.toPrecision(3);
+
+          let tomorrowTempK = weather.daily[1].temp.day;
+          let tomorrowTempC = tomorrowTempK - 273.15;
+          tomorrowTempC = tomorrowTempC.toPrecision(3);
+          
+          let weatherText = `It's ${currentTempC} degrees with ${weather.current.weather[0].description} in ${locationName}!`;
+          let weatherTomorrowText = `Tomorrow will be ${tomorrowTempC} with ${weather.daily[1].weather[0].description}`;
+          res.render('index', {weather: weatherText, weatherTomorrow: weatherTomorrowText, error: null});
+        }
+      }
+    });
+  })
+
+  
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
