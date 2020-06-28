52698e137402fb829c6f239f61c3be6a33baf874
Ubuntu <ubuntu@ip-172-31-44-109.eu-west-1.compute.internal>
2020-06-28 17:50:38 +0000

    Refactoring + Comments

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 const owmApiKey = '5badf62e6c47455b3c449f1af9635254';
 const googleApiKey = 'AIzaSyB-3hW5ZIn9ErVs27FdQBRFXQ6CTe0QJ_Y';
 
+//Days of week needed for drop down menu.
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 let d = new Date();
 let n = d.getDay();
 let dayAfter = daysOfWeek[n + 2]
@@ -24,30 +24,29 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   //let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  
   let locationurl = `https://maps.googleapis.com/maps/api/geocode/json?components=administrative_area:${city}&key=${googleApiKey}`;
 
+  //Send the user input location name to Google Geocoding API
+  //and take latitude and longitude from response.
   request(locationurl, function(err, response, body) {
     let location = JSON.parse(body);
+
+    //The API will give a 200 even if location is not found so need to check for this scenario
     if(location.status !== "OK") {
       res.render('index', {weather: null, weathericoncode: null, dayAfter: dayAfter, error: 'Error, please try again'});
     } else {
       if(err){
         res.render('index', {weather: null, weathericoncode: null, dayAfter: dayAfter, error: 'Error, please try again'});
-      } else {
-        //console.log("using google api..");
-        
+      } else { 
+        //assign latitude and longitude, and grab location name from API      
         lat = location.results[0].geometry.location.lat;
         lon = location.results[0].geometry.location.lng;
         locationName = location.results[0].address_components[0].long_name;
-        //console.log("lat is: " + lat);
-        //console.log("lon is: " + lon);
       }
   
+      //Next, call the OpenWeatherMap API using lat and lon just found
       let weatherurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${owmApiKey}`;
-  
       request(weatherurl, function (err, response, body) {
-        //console.log("Weather request. Lat is " + lat + " and lon is " + lon);
         if(err){
           res.render('index', {weather: null, weathericoncode: null, dayAfter: dayAfter, error: 'Error, please try again'});
         } else {
@@ -55,14 +54,16 @@ app.post('/', function (req, res) {
           if(weather.timezone == undefined){
             res.render('index', {weather: null, weathericoncode: null, dayAfter: dayAfter, error: 'Error, please try again'});
           } else {
+            //Convert temps from Kelvin to C, and define precision to one decimal place
             let currentTempC = (weather.current.temp - 273.15).toPrecision(3);
             let tomorrowTempC = (weather.daily[1].temp.day - 273.15).toPrecision(3);
   
+            //Concat weather icon, using the icon code from the API response.
+            //This will be passed to the HTML in EJS file.
             let weatherTodayIconUrl = `http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`;
             let weatherTomorrowIconUrl = `http://openweathermap.org/img/wn/${weather.daily[1].weather[0].icon}@2x.png`;
-  
-            //console.log("Passing the following url: " + weathericonurl);
             
+            //Concat weather text info.
             let weatherText = `It's ${currentTempC} degrees with ${weather.current.weather[0].description} in ${locationName}!`;
             let weatherTomorrowText = `Tomorrow will be ${tomorrowTempC} degrees with ${weather.daily[1].weather[0].description}`;
             res.render('index', {dayAfter: dayAfter, weatherTodayIconUrl: weatherTodayIconUrl, weatherTomorrowIconUrl: weatherTomorrowIconUrl, weather: weatherText, weatherTomorrow: weatherTomorrowText,  error: null});
